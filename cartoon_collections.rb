b0735e25412e5b3daf6fb59d43c7582f9b973e3f
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2019-09-13 19:21:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,8 @@ def roll_call_dwarves(array)
     array.map.with_index(1) { |dwarf, index| puts "#{index} #{dwarf}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  
 end
 
 def long_planeteer_calls# code an argument here

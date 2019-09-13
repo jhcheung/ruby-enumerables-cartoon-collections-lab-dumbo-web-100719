7cf40c63c64d2075484183757a1f496ad51b39ab
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2019-09-13 19:41:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,6 +14,8 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  for cheeses in cheese_types do
+    array.include
+  end 
 end

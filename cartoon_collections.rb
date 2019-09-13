5be1227423f64bb1a9212b147757eb97ec366f1a
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2019-09-13 19:37:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,10 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.find { |word| word.length > 4 }
+  if array.find { |word| word.length > 4 }
+    return true
+  end 
+  false
 end
 
 def find_the_cheese# code an argument here
